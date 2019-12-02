ff528d871dd759d2eea9f488d20e22860b52b58c
SkerrHome <sker078@aucklanduni.ac.nz>
2019-12-02 19:20:27 +1300

    making a start on the headder

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/**
- * @format
- */
+//import libraries  
+import React from 'react';
+import { View, Text, AppRegistry } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+//create component
+const App = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    );
+}
 
-AppRegistry.registerComponent(appName, () => App);
+//render component to screen
+AppRegistry.registerComponent('museum', () => App);
