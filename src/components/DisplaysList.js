769b6be97e49c83d052c323e772b5a34ea724cab
SkerrHome <sker078@aucklanduni.ac.nz>
2019-12-09 08:42:57 +1300

    using axios to pull a list of displays from an API

--- src/components/DisplaysList.js
+++ src/components/DisplaysList.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 //class always needs a render method
 class DisplaysList extends Component {
+    state = { displaysList: [] };
+
+    componentDidMount() {
+        axios.get('http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/items')
+            .then((response) => {
+                this.setState({ displaysList: response.data });
+            });
+    }
+
+    renderList = () => {
+        return this.state.displaysList.map(item => {
+            return <Text key={item.Title}>{item.Title}</Text>;
+        })
+    }
+
     render() {
+        console.log(this.state);
         return (
             <View>
-                <Text>Displays List</Text>
+                {this.renderList()}
             </View>
         );
     }
